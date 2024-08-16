e1243fc4306cd6b52e6b4a4c5cd36c263273e4c9
Jewel John <jeweljohnl0v3@gmail.com>
2024-08-16 23:31:24 +0530

    Updates

--- resources/system/character.js
+++ resources/system/character.js
@@ -3,7 +3,7 @@ import {GLTFLoader} from 'three/examples/jsm/Addons.js';
 
 //class for storing character data like animations
 export class CharacterController {
-    constructor(scene, interactionMode, minBound, maxBound) {
+    constructor(scene, minBound, maxBound) {
         this.scene = scene;
         this.model = null;
         this.pause = false;
@@ -14,7 +14,7 @@ export class CharacterController {
         this.previousActions = null;
         this.runAnimation = false;
 
-        this.interactionMode = interactionMode;
+        this.interactionMode = false;
         this.moveInput = 0;
         this.positionRef = 0;
 
@@ -50,6 +50,7 @@ export class CharacterController {
                 child.material = new THREE.MeshToonMaterial({color: 0xFFFFFF, map: child.material.map, gradientMap: gradientMap});
                 child.castShadow = true;
                 child.receiveShadow = false;
+                child.material.side = THREE.FrontSide;
             }
         });
         model.scale.set(50, 50, 50);
@@ -84,10 +85,11 @@ export class CharacterController {
             .setEffectiveWeight(1)
             .fadeIn(duration)
             .play();
-    }
-
-    //change character animation according to input
-    characterAnimation() {
+        }
+        
+        //change character animation according to input
+    characterAnimation(delta) {
+        this.mixer.update(delta);
         const insideBound = (this.positionRef > this.minBound) && (this.positionRef < this.maxBound);
         if((Math.abs(this.moveInput) > 0) && insideBound) {
             if(!this.runAnimation) {
@@ -114,9 +116,8 @@ export class CharacterController {
     }
 
     update(input, delta) {
-        this.mixer.update(delta);
-        this.moveInput = input.axes.horizontal;
+        this.moveInput = input;
         this.characterMovement(delta);
-        this.characterAnimation();
+        this.characterAnimation(delta);
     }
 }
