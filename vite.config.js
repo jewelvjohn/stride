304590a979b7ae09b787ff0042bc1660bcf81469
Jewel John <jeweljohnl0v3@gmail.com>
2024-08-16 23:22:37 +0530

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  base: '/hunted-estate/',
+    base: '/hunted-estate/',
+    build: {
+        rollupOptions: {
+            plugins: [
+                copy({
+                    targets: [{src: 'resources/**/*', dest: 'dist/resources'}],
+                    hook: 'writeBundle',
+                    flatten: false
+                })
+            ]
+        }
+    }
 })
